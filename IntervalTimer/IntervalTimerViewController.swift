7d1a5d0752b1cdbe494f65d7c1c07fa2a9b1510a
Jin Park <jinpark1504@gmail.com>
2024-06-17 05:31:17 +0900

    [Feat] Display Labels

--- IntervalTimer/IntervalTimerViewController.swift
+++ IntervalTimer/IntervalTimerViewController.swift
@@ -16,19 +16,74 @@ class IntervalTimerViewController: UIViewController {
     var restTime = 5
     var totalRounds = 3
     var currentRound = 1
+    
+    lazy var container: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 40
+        stackView.backgroundColor = .red
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    lazy var roundLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .yellow
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var secondsLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.backgroundColor = .blue
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         startWorkoutTimer()
+        
+        view.backgroundColor = .white
+        view.addSubview(container)
+        container.addArrangedSubview(roundLabel)
+        container.addArrangedSubview(titleLabel)
+        container.addArrangedSubview(secondsLabel)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            container.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            roundLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+            roundLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+            titleLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+            secondsLabel.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+            secondsLabel.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+            
+        ])
     }
 
     func startWorkoutTimer() {
-        print("Round \(self.currentRound)")
-        print("\(self.workoutTime) seconds")
+        self.roundLabel.text = "Round \(self.currentRound) / \(self.totalRounds)"
+        self.titleLabel.text  = "Workout"
+        self.secondsLabel.text = "\(self.workoutTime) seconds"
         workoutTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
             guard let self = self else { return }
             self.workoutTime -= 1
-            print("\(self.workoutTime) seconds")
+            self.secondsLabel.text = "\(self.workoutTime) seconds"
             if self.workoutTime == 0 {
                 self.workoutTimer?.invalidate()
                 self.workoutTime = self.initialWorkoutTIme
@@ -38,11 +93,12 @@ class IntervalTimerViewController: UIViewController {
     }
     
     func startRestTimer() {
-        print("\(self.restTime) seconds (Rest)")
+        self.titleLabel.text  = "Rest"
+        self.secondsLabel.text = "\(self.restTime) seconds (Rest)"
         restTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
             guard let self = self else { return }
             self.restTime -= 1
-            print("\(self.restTime) seconds (Rest)")
+            self.secondsLabel.text = "\(self.restTime) seconds (Rest)"
             if self.restTime == 0 {
                 self.restTimer?.invalidate()
                 self.restTime = self.initialRestTime
@@ -57,7 +113,8 @@ class IntervalTimerViewController: UIViewController {
     }
     
     func endWorkout() {
-        print("Workout End")
+        self.titleLabel.text  = "Finish"
+        self.secondsLabel.text = nil
     }
 }
 
